6c7742b1855c040f2b28aa30db0ee31b0946e5e8
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-06-03 03:04:57 +0200

    cleanup

--- NTR/utils/thermoFunctions.py
+++ NTR/utils/thermoFunctions.py
@@ -1,7 +1,8 @@
-def Sutherland_Law(T,As=1.458e-06,Ts=110.4):
-    nu=As*(T)**(0.5)/(1+Ts/T)
+def Sutherland_Law(T, As=1.458e-06, Ts=110.4):
+    nu = As * (T) ** (0.5) / (1 + Ts / T)
     return nu
 
-def TByIdealGasLaw(rho,p,R=287.058):
-    T=p/(rho*R)
+
+def TByIdealGasLaw(rho, p, R=287.058):
+    T = p / (rho * R)
     return T
