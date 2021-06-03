ef475c8be4b563495337448ab00dbd30a210df51
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-06-03 03:09:43 +0200

    cleanup

--- NTR/utils/case.py
+++ NTR/utils/case.py
@@ -1,31 +1,29 @@
 import os
 
-from NTR.utils.coefficients import fluid_coeffs
+from NTR.utils.coefficients import FluidCoeffs
 
 
-class abstract_case:
+class AbstractCase:
     def __init__(self, name):
         self.name = name
         self.mesh_dict = {}
-        self.fluid_coeffs = fluid_coeffs()
+        self.fluid_coeffs = FluidCoeffs()
 
     def set_mesh(self, name, path_to_mesh):
         abspath = os.path.abspath(path_to_mesh)
         self.mesh_dict[name] = abspath
 
 
-class cascade_case(abstract_case):
+class CascadeCase(AbstractCase):
     def __init__(self, name):
         super().__init__(name)
-        self.x_pos = {"x_pos1":None,
-                      "x_pos2":None}
+        self.x_pos = {"x_pos1": None,
+                      "x_pos2": None}
 
-    def set_x_pos(self,x_pos1,x_pos2):
+    def set_x_pos(self, x_pos1, x_pos2):
 
         assert type(x_pos1) == float, "x_pos1 needs to be a float"
         assert type(x_pos2) == float, "x_pos2 needs to be a float"
 
         self.x_pos["x_pos1"] = x_pos1
         self.x_pos["x_pos2"] = x_pos2
-
-
