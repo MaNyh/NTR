6bc5edbbd11c8d13327d6080a5fbf46966509265
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-06-15 16:52:12 +0200

    quick reimplementation of dead_examples. need to be cleaned asap

--- examples/CascadeCase_gwk_les/run_postprocess.py
+++ examples/CascadeCase_gwk_les/run_postprocess.py
@@ -1,5 +1,5 @@
 from NTR.utils.casereader import case_from_dict
 from NTR.postprocessing.createProfileData import createProfileData
 
-c = case_from_dict("gwk_verdichtercascade_LES.yml")
+c = case_from_dict("postprocessing_settings.yml")
 createProfileData(c)
--- examples/CascadeCase_gwk_les/run_probecreator.py
+++ examples/CascadeCase_gwk_les/run_probecreator.py
@@ -1,12 +1,12 @@
 from NTR.preprocessing.openfoam.create_probes import createProbesProfileDict, createProbesStreamlineDict
 from NTR.utils.case import CascadeCase
 
-case = CascadeCase("GWK_Verdichterkaskade_LES")
+case = CascadeCase("GWK_Verdichterkaskade_LES","openfoam_timeaveraged", "placeholder_simtype")
 
 case.set_casedir(".")
 
-case.set_mesh("fluid", "ressources/VTK/05_GWKVD_109000.vtk")
-case.set_mesh("blade", "ressources/VTK/BLADE/BLADE_109000.vtk")
+case.set_mesh("fluid", "vtk_results/VTK/06_GWKVD_317000.vtk")
+case.set_mesh("blade", "vtk_results/VTK/BLADE/BLADE_317000.vtk")
 case.load_mesh_dict()
 
 case.CascadeCoeffs.set_alpha(0.01)
