8f00bfc407564a452b3a8a618705ecd7140128f2
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-06-28 17:19:54 +0200

    swap unittest with pytest

--- tests/test_NTR.py
+++ tests/test_NTR.py
@@ -3,31 +3,49 @@
 """Tests for `NTR` package."""
 
 
-import unittest
+import pytest
 from click.testing import CliRunner
 
 from NTR import NTR
 from NTR import cli
 
+from NTR.utils.functions import yaml_dict_read
 
-class TestNtr(unittest.TestCase):
-    """Tests for `NTR` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
-    def test_000_something(self):
-        """Test something."""
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'NTR.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+
+def test_command_line_interface():
+    """Test the CLI."""
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'tgtk.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_yamlDictRead(tmpdir):
+    """
+    tmpdir ist eine spezialvariable die durch pytest erkannt wird (ist ein PYTHONPATH-objekt)
+    """
+    test_file = tmpdir / "test.yaml"
+    with open(test_file, "w") as handle:
+        handle.write("test_key: True\n")
+    assert yaml_dict_read(test_file) == {"test_key": True}
+
+
+
