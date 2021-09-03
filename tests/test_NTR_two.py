b4bfe598284524ae367a8cca3be42fdae3ac62d8
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-09-03 23:03:08 +0200

    cleanup

--- tests/test_NTR_two.py
+++ tests/test_NTR_two.py
@@ -6,20 +6,22 @@ from NTR.utils.functions import run_igg_meshfuncs, yaml_dict_read
 from NTR.preprocessing.create_simcase import create_simulationcase
 
 ntrpath = os.path.abspath(os.path.dirname(NTR.__file__))
-examples_compressor_kurth = os.path.join(ntrpath,"..", "examples", "CascadeCase_compressor_kurth", "case_settings.yml")
-examples_gwkles = os.path.join(ntrpath,"..", "examples", "CascadeCase_gwk_les", "case_settings.yml")
-examples_gwkras = os.path.join(ntrpath,"..", "examples", "CascadeCase_gwk_rans", "case_settings.yml")
-examples_naca6510 = os.path.join(ntrpath,"..", "examples", "CascadeCase_gwk_naca6510", "case_settings.yml")
-examples_nacagen = os.path.join(ntrpath,"..", "examples", "CascadeCase_NACA_airfoilgenerator", "case_settings.yml")
-examples_turbine_seehausen = os.path.join(ntrpath,"..", "examples", "CascadeCase_turbine_seehausen", "case_settings.yml")
-examples_gwkras_trace = os.path.join(ntrpath,"..", "examples", "CascadeCase_gwk_rans_trace", "case_settings.yml")
+examples_compressor_kurth = os.path.join(ntrpath, "..", "examples", "CascadeCase_compressor_kurth", "case_settings.yml")
+examples_gwkles = os.path.join(ntrpath, "..", "examples", "CascadeCase_gwk_les", "case_settings.yml")
+examples_gwkras = os.path.join(ntrpath, "..", "examples", "CascadeCase_gwk_rans", "case_settings.yml")
+examples_naca6510 = os.path.join(ntrpath, "..", "examples", "CascadeCase_gwk_naca6510", "case_settings.yml")
+examples_nacagen = os.path.join(ntrpath, "..", "examples", "CascadeCase_NACA_airfoilgenerator", "case_settings.yml")
+examples_turbine_seehausen = os.path.join(ntrpath, "..", "examples", "CascadeCase_turbine_seehausen",
+                                          "case_settings.yml")
+examples_gwkras_trace = os.path.join(ntrpath, "..", "examples", "CascadeCase_gwk_rans_trace", "case_settings.yml")
 
-igg_settings = yaml_dict_read(os.path.join(ntrpath,"utils","externals","externals_settings.yml"))
+igg_settings = yaml_dict_read(os.path.join(ntrpath, "utils", "externals", "externals_settings.yml"))
 iggdir = igg_settings["igg"]["install_directory"]
 igginstance = igg_settings["igg"]["executable"]
 iggpath = os.path.join(iggdir, igginstance)
 
 ON_CI = 'CI' in os.environ
+
 
 @pytest.mark.skipif(ON_CI, reason="do not run in continuous integration environment due to limited ressources")
 def test_example_compressor_kurth():
@@ -29,6 +31,7 @@ def test_example_compressor_kurth():
     else:
         print("igg-path is not right")
 
+
 @pytest.mark.skipif(ON_CI, reason="do not run in continuous integration environment due to limited ressources")
 def test_example_gwkles():
     if os.path.isfile(iggpath):
@@ -36,6 +39,7 @@ def test_example_gwkles():
     else:
         print("igg-path is not right")
     create_simulationcase(examples_gwkles)
+
 
 @pytest.mark.skipif(ON_CI, reason="do not run in continuous integration environment due to limited ressources")
 def test_example_gwkras():
@@ -46,6 +50,7 @@ def test_example_gwkras():
         print("igg-path is not right")
     create_simulationcase(examples_gwkras)
 
+
 @pytest.mark.skipif(ON_CI, reason="do not run in continuous integration environment due to limited ressources")
 def test_example_naca6510():
     run_create_geometry(examples_naca6510)
@@ -53,6 +58,7 @@ def test_example_naca6510():
         run_igg_meshfuncs(examples_naca6510)
     else:
         print("igg-path is not right")
+
 
 @pytest.mark.skipif(ON_CI, reason="do not run in continuous integration environment due to limited ressources")
 def test_example_nacagen():
@@ -62,6 +68,7 @@ def test_example_nacagen():
     else:
         print("igg-path is not right")
 
+
 @pytest.mark.skipif(ON_CI, reason="do not run in continuous integration environment due to limited ressources")
 def test_example_turbine_seehausen():
     run_create_geometry(examples_turbine_seehausen)
@@ -69,6 +76,7 @@ def test_example_turbine_seehausen():
         run_igg_meshfuncs(examples_turbine_seehausen)
     else:
         print("igg-path is not right")
+
 
 @pytest.mark.skipif(ON_CI, reason="do not run in continuous integration environment due to limited ressources")
 def test_example_gwkras_trace():
