31f78e0c3c9fd6ecab4e9b9f7fb121df30842a93
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-09-03 13:51:50 +0200

    updating CascadeCase_rans_les

--- NTR/database/case_templates/openfoam_cascade_les/submit_job_rrzn.sh
+++ NTR/database/case_templates/openfoam_cascade_les/submit_job_rrzn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=fLE_highBD_rotating2012_roundTE
+#SBATCH --job-name=<var CLUSTERJOBNAME var>
 #SBATCH --nodes=<var NODES var>
 #SBATCH --ntasks-per-node=<var TASKS_PER_NODE var>
 #SBATCH --mem-per-cpu=2G
@@ -12,9 +12,7 @@
 
 . $MODULESHOME/init/ksh
 source ~/ModuleLoading/OpenFoam1612.sh
-HOST=$(paste -s -d ',' ${PBS_NODEFILE})
-echo $HOST
-echo $PBS_NODEFILE
-cd $PBS_O_WORKDIR
 
-mpirun -np <var PROCS var> rhoPimpleFoam -case ./ -parallel >& log.log
+cd $SLURM_SUBMIT_DIR
+
+mpirun -np <var PROCS var> rhoPimpleFoam -case ./ -parallel > log.log
