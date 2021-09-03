38158cd97690c8fbf4d1846905579cb17d9ebe68
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-09-03 12:43:18 +0200

    updating CascadeCase_rans_les

--- NTR/database/case_templates/openfoam_cascade_les/submit_job_hlrn.sh
+++ NTR/database/case_templates/openfoam_cascade_les/submit_job_hlrn.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -A <var HLRN_JOB_ACCOUNT var>
-#SBATCH --job-name=P14_start
+#SBATCH --job-name=<var CLUSTERJOBNAME var>
 #SBATCH -t <var RUNTIME \>
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=<var HLRN_JOB_EMAIL var>
-#SBATCH --nodes=__NODES__
-#SBATCH --tasks-per-node=96
+#SBATCH --nodes=<var NODES var>
+#SBATCH --tasks-per-node=<var TASKS_PER_NODE var>
 #SBATCH -p standard96
 
 export TMPDIR=$LOCAL_TMPDIR
 
 module load  gcc/7.5.0 cmake/3.16.2; source /home/niikceng/OpenFOAM/OpenFOAM-v1612+/etc/bashrc
 export FOAM_USER_PATH='$HOME/OpenFOAM/niinyhma-v1612+/'
-mpirun -mca btl '^openib' -np __PROCS__ rhoPimpleFoam -parallel -case ./ > log.log
+mpirun -mca btl '^openib' -np <var PROCS var> rhoPimpleFoam -parallel -case ./ > log.log
 
---
+++ NTR/database/case_templates/openfoam_cascade_les/submit_job_rrzn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --job-name=fLE_highBD_rotating2012_roundTE
+#SBATCH --nodes=<var NODES var>
+#SBATCH --ntasks-per-node=<var TASKS_PER_NODE var>
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=100:00:00
+#SBATCH --constraint=[skylake|haswell]
+#SBATCH --mail-user=<var HLRN_JOB_EMAIL var>
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output batch.out
+#SBATCH --error batch.err
+
+. $MODULESHOME/init/ksh
+source ~/ModuleLoading/OpenFoam1612.sh
+HOST=$(paste -s -d ',' ${PBS_NODEFILE})
+echo $HOST
+echo $PBS_NODEFILE
+cd $PBS_O_WORKDIR
+
+mpirun -np <var PROCS var> pimpleFoam -case ./ -parallel >& log.log
