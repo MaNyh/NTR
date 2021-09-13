cbf965e9bb9e5873aac1d216742a1817ee2383ab
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-09-13 10:32:15 +0200

    addint parametrization for openfoam ras

--- NTR/database/case_templates/openfoam_cascade_ras/rrzn.sh
+++ NTR/database/case_templates/openfoam_cascade_ras/rrzn.sh
@@ -1,17 +1,16 @@
-#PBS -S /bin/ksh 
-#PBS -N TEST_mappedDFSEM__PipeFlow 
-#PBS -j oe 
-#PBS -l nodes=1:ppn=12
-#PBS -l walltime=10:00:00,mem=16Gb 
-#PBS -M nyhuis@tfd.uni-hannover.de 
-#PBS -m ae 
+#PBS -S /bin/ksh
+#PBS -N TEST_mappedDFSEM__PipeFlow
+#PBS -j oe
+#PBS -l nodes=1:ppn=<var PROCS var>
+#PBS -l walltime=10:00:00,mem=16Gb
+#PBS -M nyhuis@tfd.uni-hannover.de
+#PBS -m ae
 #PBS -W x=PARTITION:taurus:haku:lena
-. $MODULESHOME/init/ksh 
-source ~/ModuleLoading/OpenFoam1612.sh 
-HOST=$(paste -s -d ',' ${PBS_NODEFILE}) 
-echo $HOST 
+. $MODULESHOME/init/ksh
+source ~/ModuleLoading/OpenFoam1612.sh
+HOST=$(paste -s -d ',' ${PBS_NODEFILE})
+echo $HOST
 echo $PBS_NODEFILE
-
-cd $PBS_O_WORKDIR 
-mpirun --hostfile ${PBS_NODEFILE} -np 12 rhoPimpleFoam -parallel > ${PBS_JOBNAME}.${PBS_JOBID}.out
+cd $PBS_O_WORKDIR
+mpirun --hostfile ${PBS_NODEFILE} -np <var PROCS var> rhoPimpleFoam -parallel > ${PBS_JOBNAME}.${PBS_JOBID}.out
 #rhoPimpleFoam > ssp.${PBS_JOBNAME}.${PBS_JOBID}.out
