e29c9ee160a9faeb8acf775bc4f9fd0044450f9e
Malte Nyhuis <nyhuis@tfd.uni-hannover.de>
2021-09-17 09:36:39 +0200

    new job-scripts for pbs and slurm in les channel-case (openfoam)

---
+++ NTR/database/case_templates/openfoam_channel_les/submit_job_rrzn_slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --job-name=<var CLUSTERJOBNAME var>
+#SBATCH --nodes=<var NODES var>
+#SBATCH --ntasks-per-node=<var TASKS_PER_NODE var>
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=<var WALLTIME var>
+#SBATCH --constraint=[skylake|haswell]
+#SBATCH --mail-user=<var JOB_EMAIL var>
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output batch.out
+#SBATCH --error batch.err
+
+module load GCC/4.9.3-2.25 OpenMPI/1.10.2 OpenFOAM/v1612+
+source $FOAM_BASH
+cd $SLURM_SUBMIT_DIR
+
+mpirun  -np <var JOB_NUMBERS var> pimpleFoam -case ./ -parallel > log.log
