2bd0d8d8b100a2344ba44f8d76fcf16598d21f55
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-10-07 18:01:00 +0200

    workaround. this commit has to be re-made. paraview needs to export ALL arrays when converting from cgns to vtk

--- NTR/utils/externals/paraview/paraview_template_cgns_to_vtk.py
+++ NTR/utils/externals/paraview/paraview_template_cgns_to_vtk.py
@@ -12,6 +12,10 @@ paraview.simple._DisableFirstRenderCameraReset()
 # create a new 'CGNS Series Reader'
 tRACEcgns = CGNSSeriesReader(FileNames=[os.path.join(os.path.dirname(__file__),"tmp.cgns")])
 
+print(tRACEcgns.CellArrayInfo())
+
+tRACEcgns.CellArrayStatus = ['Density', 'Mach', 'Pressure', 'Temperature', 'TurbulentDissipationRate', 'TurbulentDistance', 'TurbulentEnergyKinetic', 'Velocity', 'ViscosityEddy', 'ViscosityEddyRatio']
+
 # create a new 'Merge Blocks'
 mergeBlocks1 = MergeBlocks(Input=tRACEcgns)
 
