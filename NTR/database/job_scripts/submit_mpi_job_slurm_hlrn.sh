43cf51f6cfb0336134a3b05d2400b7b16cc6d7b7
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-10-13 23:04:12 +0200

    redefining job-scripts to a generic type

--- NTR/database/job_scripts/submit_mpi_job_slurm_hlrn.sh
+++ NTR/database/job_scripts/submit_mpi_job_slurm_hlrn.sh
@@ -10,7 +10,6 @@
 
 export TMPDIR=$LOCAL_TMPDIR
 
-module load  gcc/7.5.0 cmake/3.16.2; source /home/niikceng/OpenFOAM/OpenFOAM-v1612+/etc/bashrc
-export FOAM_USER_PATH='$HOME/OpenFOAM/niinyhma-v1612+/'
-mpirun -mca btl '^openib' -np <var PROCS var> rhoPimpleFoam -parallel -case ./ > log.log
+<var JOB_SOURCECMD var>
 
+mpirun -mca btl '^openib' -np <var PROCS var> rhoPimpleFoam -parallel -case ./ > log.log
--- NTR/database/job_scripts/submit_sp_pbs_rrzn.sh
+++ NTR/database/job_scripts/submit_sp_pbs_rrzn.sh
@@ -1,7 +1,7 @@
 #PBS -S /bin/ksh
 #PBS -N TEST_mappedDFSEM__PipeFlow
 #PBS -j oe
-#PBS -l nodes=1:ppn=<var PROCS var>
+#PBS -l nodes=1:ppn=1
 #PBS -l walltime=10:00:00,mem=16Gb
 #PBS -M nyhuis@tfd.uni-hannover.de
 #PBS -m ae
@@ -9,8 +9,6 @@
 . $MODULESHOME/init/ksh
 source ~/ModuleLoading/OpenFoam1612.sh
 HOST=$(paste -s -d ',' ${PBS_NODEFILE})
-echo $HOST
-echo $PBS_NODEFILE
+
 cd $PBS_O_WORKDIR
-mpirun --hostfile ${PBS_NODEFILE} -np <var PROCS var> rhoPimpleFoam -parallel > ${PBS_JOBNAME}.${PBS_JOBID}.out
-#rhoPimpleFoam > ssp.${PBS_JOBNAME}.${PBS_JOBID}.out
+rhoPimpleFoam > log.log
