db1ae11b49066c78d7d25d1abe546e7f79392183
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-10-15 02:02:45 +0200

    forming structure

--- NTR/postprocessing/transient.py
+++ NTR/postprocessing/transient.py
@@ -1,47 +1,78 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+class signal_generator:
+    def __init__(self):
+        self.datalen = 10000
+        self.date_rng = np.arange(0, self.datalen)
+
+        self.sin_lasting = np.random.randint(0, 100)
+        self.tanh_lasting = np.random.randint(0, 100)
+        self.time = 100
+
+        self.timesteps = np.arange(0, self.time, self.datalen ** -1)
+
+    def tanh_signal(self):
+        """
+        :param time:
+        :return:
+        """
+
+        turn = np.random.choice([-1, 1])
+        ans = np.sinh(self.timesteps * self.tanh_lasting ** -1) / np.cosh(self.timesteps * self.tanh_lasting ** -1)
+
+        stationarity = ans
+        return ans * turn, stationarity
+
+    def sin_signal(self, oscilation_duration):
+        abate = np.e ** (-self.timesteps * oscilation_duration ** -1)
+        abate /= max(abate)
+        sinus = np.sin(self.timesteps) * abate
+        return sinus, 1 - abate
+
+    def noise(self):
+        return np.random.rand(len(self.timesteps))
+
+    def generate(self):
+        sinus, stat_sin = self.sin_signal(self.sin_lasting)
+        tanh, stat_tanh = self.tanh_signal()
+        rausch = self.noise()
+
+        if stat_sin[-1] < 0.95:
+            print("sin still transient ", str(stat_sin[-1]))
+        if stat_tanh[-1] < 0.95:
+            print("tanh still transient ", str(stat_tanh[-1]))
+
+        signal = sinus + tanh + rausch
+
+        return sinus, tanh, rausch, signal, stat_sin , stat_tanh
+
+    def plot(self):
+        fig, axs = plt.subplots(6, 1)
+        sinus, tanh, rausch, signal, stat_sin , stat_tanh = self.generate()
+        axs[0].plot(np.arange(0, self.time, self.datalen ** -1), sinus)
+        axs[1].plot(np.arange(0, self.time, self.datalen ** -1), stat_sin)
+        axs[2].plot(np.arange(0, self.time, self.datalen ** -1), tanh)
+        axs[3].plot(np.arange(0, self.time, self.datalen ** -1), stat_tanh)
+        axs[4].plot(np.arange(0, self.time, self.datalen ** -1), rausch)
+        axs[5].plot(np.arange(0, self.time, self.datalen ** -1), signal)
+
+        plt.show()
+
+
 def test_transientcheck():
+    siggen = signal_generator()
+    siggen.plot()
     return 0
+
 
 def transientcheck():
     return 0
+
 
 def signalcreator():
     return 0
 
 
-datalen = 1000
-amp = 10
-oscilation_duration = 10
-date_rng = np.arange(0,datalen)
-
-time = np.arange(0, datalen)/10
-
-
-def tanh(time):
-    stationary = np.random.rand()*2-1
-    zaehl = 1#np.random.rand()
-    nenn = np.e**(2*time*np.random.rand()) - 1
-    ans = stationary + zaehl/nenn
-    ans /= max(abs(ans))
-    return ans
-
-sinus = np.sin(time) * np.e**(-time*oscilation_duration**-1)*amp
-tanh = tanh(time)
-rausch = np.random.rand(datalen)
-
-signal= sinus+tanh+rausch
-#print(signal)
-fig, axs = plt.subplots(2, 1)
-
-axs[1].plot(time,signal)
-axs[0].plot(time,tanh+sinus)
-
-def mean(series):
-    sum = np.sum(series)
-    return sum/len(series)
-
-
-
-plt.show()
+transientcheck()
