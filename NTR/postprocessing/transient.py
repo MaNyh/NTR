37f9aa3cee6bbf8e8f463568ea934a711395ab45
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-10-16 14:52:01 +0200

    refactoring for analytical solution

--- NTR/postprocessing/transient.py
+++ NTR/postprocessing/transient.py
@@ -3,53 +3,53 @@ import matplotlib.pyplot as plt
 
 
 class signal_generator:
-    def __init__(self):
-        self.datalen = 10000
-        self.date_rng = np.arange(0, self.datalen)
+    datalen = 10000 # length of timeseries
+    transientlimit = 0.95
 
+    def __init__(self):
         self.sin_lasting = np.random.randint(0, 100)
         self.tanh_lasting = np.random.randint(0, 100)
         self.time = 100
 
+        self.turn = np.random.choice([-1, 1])
+
         self.timesteps = np.arange(0, self.time, self.datalen ** -1)
 
-        self.transientlimit = 0.95
+        self.sinabate =np.e ** (-self.timesteps * self.sin_lasting ** -1)
+        self.sinabate /= max(self.sinabate)
+
+        self.sin_stationary = np.argmax(self.sinabate<(1-self.transientlimit))
+        self.tanh_stationarity = np.argmax(np.sinh(self.timesteps * self.tanh_lasting ** -1) / np.cosh(self.timesteps * self.tanh_lasting ** -1)>self.transientlimit)
 
     def tanh_signal(self):
-        """
-        :param time:
-        :return:
-        """
-
-        turn = np.random.choice([-1, 1])
         ans = np.sinh(self.timesteps * self.tanh_lasting ** -1) / np.cosh(self.timesteps * self.tanh_lasting ** -1)
+        return ans * self.turn
 
-        stationarity = ans
-        return ans * turn, stationarity
+    def sin_signal(self):
+        sinus = np.sin(self.timesteps) * self.sinabate
+        return sinus
 
-    def sin_signal(self, oscilation_duration):
-        abate = np.e ** (-self.timesteps * oscilation_duration ** -1)
-        abate /= max(abate)
-        sinus = np.sin(self.timesteps) * abate
-        return sinus, 1 - abate
-
-    def noise(self):
+    def noise_signal(self):
         return np.random.rand(len(self.timesteps))*np.random.rand()
 
     def generate(self):
-        sinus, stat_sin = self.sin_signal(self.sin_lasting)
-        tanh, stat_tanh = self.tanh_signal()
-        rausch = self.noise()
+        sinus = self.sin_signal()
+        tanh = self.tanh_signal()
+        rausch = self.noise_signal()
 
-        if stat_sin[-1] < self.transientlimit:
-            print("sin still transient ", str(stat_sin[-1]))
-        if stat_tanh[-1] < self.transientlimit:
-            print("tanh still transient ", str(stat_tanh[-1]))
-
+        sin_stats = (-1 + self.sinabate)*-1
+        tanh_stats = np.sinh(self.timesteps * self.tanh_lasting ** -1) / np.cosh(self.timesteps * self.tanh_lasting ** -1)
+        if sin_stats[-1] < self.transientlimit:
+            print("sin still transient ", str(sin_stats[-1]))
+        if tanh_stats[-1] < self.transientlimit:
+            print("tanh still transient ", str(tanh_stats[-1]))
 
         signal = sinus + tanh + rausch
 
-        return sinus, tanh, rausch, signal, stat_sin , stat_tanh
+        return sinus, tanh, rausch, signal, sin_stats , tanh_stats
+
+    def analytical_stat_solution(self):
+        return 0
 
     def plot(self,sinus, tanh, rausch, signal, stat_sin , stat_tanh):
         fig, axs = plt.subplots(6, 1)
@@ -66,15 +66,17 @@ class signal_generator:
 
 def test_transientcheck():
     siggen=signal_generator()
+    infval = siggen.turn
     sinus, tanh, rausch, signal, stat_sin , stat_tanh = siggen.generate()
     siggen.plot(sinus, tanh, rausch, signal, stat_sin , stat_tanh)
+
     return 0
 
 
 def transientcheck():
     """
+    this method must check when a steady-state is
     https://link.springer.com/content/pdf/10.1007/s00162-018-0474-0.pdf
-    :return:
     """
     return 0
 
