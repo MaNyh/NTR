f867bc8daf92d20ef27afbb110350293de5c7509
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-10-16 20:31:02 +0200

    adding legends and colors in plot refactorization and cleanup

--- NTR/postprocessing/transient.py
+++ NTR/postprocessing/transient.py
@@ -9,34 +9,52 @@ class signal_generator:
     def __init__(self):
         self.sin_lasting = np.random.randint(0, 100)
         self.tanh_lasting = np.random.randint(0, 100)
-        self.time = 100
+        self.sin_stationary_ts = -self.sin_lasting * (1 + np.log(1 - self.transientlimit))
+        self.tanh_stationary_ts = np.arctanh(self.transientlimit) * self.tanh_lasting
 
-        self.turn = np.random.choice([-1, 1])
+        if self.sin_stationary_ts > self.tanh_stationary_ts:
+            self.time = 2*self.sin_stationary_ts
+        else:
+            self.time = 2*self.tanh_stationary_ts
 
         self.timesteps = np.arange(0, self.time, self.datalen ** -1)
 
-        self.sinabate =np.e ** (-self.timesteps * self.sin_lasting ** -1) / np.e
+        self.sin_abate = np.e ** (-self.timesteps * self.sin_lasting ** -1) / np.e
+        self.tanh_sign = np.random.choice([-1, 1])
+        self.sin_stationary = np.argmax(self.sin_abate < (1 - self.transientlimit))
+        self.tanh_stationarity = np.argmax( np.tanh(self.timesteps * self.tanh_lasting ** -1) > self.transientlimit)
 
-        self.sin_stationary = np.argmax(self.sinabate<(1-self.transientlimit))
-        self.tanh_stationarity = np.argmax(np.sinh(self.timesteps * self.tanh_lasting ** -1) / np.cosh(self.timesteps * self.tanh_lasting ** -1)>self.transientlimit)
+        print()
+        print("time when stationary")
+        ts = self.sin_stationary_ts
+        ss = self.tanh_stationary_ts
+        print("sin : ", ts)
+        print("tanh : ", ss)
+        stat_time = ts if ts > ss else ss
+        print("signal stationary at", stat_time)
+        print("this equals a timestep of ", int(stat_time/self.time*len(self.timesteps)), " from ", len(self.timesteps) , " or " , round(int(stat_time/self.time*len(self.timesteps))/len(self.timesteps),1))
+
 
     def tanh_signal(self):
-        ans = np.sinh(self.timesteps * self.tanh_lasting ** -1) / np.cosh(self.timesteps * self.tanh_lasting ** -1)
-        return ans * self.turn
+        ans = np.tanh(self.timesteps * self.tanh_lasting ** -1)
+        return ans * self.tanh_sign
 
     def sin_signal(self):
-        sinus = np.sin(self.timesteps) * self.sinabate
+        sinus = np.sin(self.timesteps) * self.sin_abate
         return sinus
 
     def noise_signal(self):
-        return np.random.rand(len(self.timesteps))*np.random.rand()
+        mu, sigma = 0, 1 # mean and standard deviation
+        s = np.random.normal(mu, sigma, size=len(self.timesteps))
+        s /= max(s)
+        return s
 
     def generate(self):
         sinus = self.sin_signal()
         tanh = self.tanh_signal()
         rausch = self.noise_signal()
 
-        sin_stats = (-1 + self.sinabate)*-1
+        sin_stats = (-1 + self.sin_abate) * -1
         tanh_stats = np.sinh(self.timesteps * self.tanh_lasting ** -1) / np.cosh(self.timesteps * self.tanh_lasting ** -1)
         if sin_stats[-1] < self.transientlimit:
             print("sin still transient ", str(sin_stats[-1]))
@@ -47,28 +65,29 @@ class signal_generator:
 
         return sinus, tanh, rausch, signal, sin_stats , tanh_stats
 
-    def analytical_stat_solution(self):
-        return 0
 
     def plot(self,sinus, tanh, rausch, signal, stat_sin , stat_tanh):
         fig, axs = plt.subplots(6, 1)
 
-        axs[0].plot(np.arange(0, self.time, self.datalen ** -1), sinus)
-        axs[1].plot(np.arange(0, self.time, self.datalen ** -1), stat_sin)
-        axs[2].plot(np.arange(0, self.time, self.datalen ** -1), tanh)
-        axs[3].plot(np.arange(0, self.time, self.datalen ** -1), stat_tanh)
-        axs[4].plot(np.arange(0, self.time, self.datalen ** -1), rausch)
-        axs[5].plot(np.arange(0, self.time, self.datalen ** -1), signal)
+        axs[0].plot(np.arange(0, self.time, self.datalen ** -1), sinus, color="orange", label="abating sine signal")
+        axs[1].plot(np.arange(0, self.time, self.datalen ** -1), stat_sin, color="orange", label="stationarity sine signal")
+        axs[2].plot(np.arange(0, self.time, self.datalen ** -1), tanh, color="blue", label="tanh signal")
+        axs[3].plot(np.arange(0, self.time, self.datalen ** -1), stat_tanh, color="blue", label="stationarity tanh signal")
+        axs[4].plot(np.arange(0, self.time, self.datalen ** -1), rausch, color="black", label="noise")
+        axs[5].plot(np.arange(0, self.time, self.datalen ** -1), signal, color="red",label="signal")
+
+        axs[0].legend(loc="upper right")
+        axs[1].legend(loc="upper right")
+        axs[2].legend(loc="upper right")
+        axs[3].legend(loc="upper right")
+        axs[4].legend(loc="upper right")
+        axs[5].legend(loc="upper right")
 
         plt.show()
 
 
 def test_transientcheck():
     siggen=signal_generator()
-
-    sin_stationary_ts = -siggen.sin_lasting*(1+np.log(1-siggen.transientlimit))
-    tanh_stationary_ts = np.arctanh(siggen.transientlimit)*siggen.tanh_lasting
-
 
     sinus, tanh, rausch, signal, stat_sin , stat_tanh = siggen.generate()
     siggen.plot(sinus, tanh, rausch, signal, stat_sin , stat_tanh)
