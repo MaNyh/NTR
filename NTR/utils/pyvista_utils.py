9ec76ff92b27e9ee4f0ca2eba10c5784f50730c6
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-10-19 17:18:44 +0200

    refactoring

--- NTR/utils/pyvista_utils.py
+++ NTR/utils/pyvista_utils.py
@@ -64,7 +64,7 @@ def constructWallMesh(meshList):
     return wallMesh
 
 
-def calc_dist_from_surface(surface_one, surface_two, verbose=False):
+def calc_dist_from_surface(surface_primary, surface_secondary, verbose=False):
     """
     Distance Between Two Surfaces / A Surface and a Pointcloud
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -75,10 +75,10 @@ def calc_dist_from_surface(surface_one, surface_two, verbose=False):
     methods. We will demo a method where we compute the normals of the
     bottom surface, and then project a ray to the top surface to compute the
     distance along the surface normals.
-    :param surface_one: pv.UnstructuredGrid
-    :param surface_two: pv.UnstructuredGrid / pv.PolyData
+    :param surface_primary: pv.UnstructuredGrid
+    :param surface_secondary: pv.UnstructuredGrid / pv.PolyData
     :param verbose: plots?
-    :return: surface with distance
+    :return: surface_primary with distances from secondary
     """
 
     ###############################################################################
@@ -86,20 +86,20 @@ def calc_dist_from_surface(surface_one, surface_two, verbose=False):
     # ++++++++++++++++++++
     #
 
-    h0n = surface_one.compute_normals(point_normals=True, cell_normals=False,
-                                      auto_orient_normals=True)
+    h0n = surface_primary.compute_normals(point_normals=True, cell_normals=False,
+                                          auto_orient_normals=True)
 
     ###############################################################################
     # Travel along normals to the other surface and compute the thickness on each
     # vector.
 
-    h0n["distances"] = np.empty(surface_one.n_points)
+    h0n["distances"] = np.empty(surface_primary.n_points)
     for i in range(h0n.n_points):
         p = h0n.points[i]
         vec = h0n["Normals"][i] * h0n.length
         p0 = p - vec
         p1 = p + vec
-        ip, ic = surface_two.ray_trace(p0, p1, first_point=True)
+        ip, ic = surface_secondary.ray_trace(p0, p1, first_point=True)
         dist = np.sqrt(np.sum((ip - p) ** 2))
         h0n["distances"][i] = dist
 
@@ -113,8 +113,8 @@ def calc_dist_from_surface(surface_one, surface_two, verbose=False):
         ###############################################################################
         p = pv.Plotter()
         p.add_mesh(h0n, scalars="distances", smooth_shading=True)
-        p.add_mesh(surface_two, color=True, opacity=0.75, smooth_shading=True)
+        p.add_mesh(surface_secondary, color=True, opacity=0.75, smooth_shading=True)
         p.show()
 
 
-    return surface_one
+    return h0n
