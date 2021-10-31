6e651db88dc1713dc1a3842d9cc52a50b5594aa2
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-10-31 19:34:52 +0100

    keep "gradient" as an unmanipulated object

--- NTR/postprocessing/lambdatwomethod.py
+++ NTR/postprocessing/lambdatwomethod.py
@@ -2,11 +2,10 @@ import pyvista as pv
 import numpy as np
 
 from NTR.utils.pyvista_utils import load_mesh
-mesh = load_mesh(r"D:\NTR\examples\ChannelCase_les\03_Solution\VTK\02_Simcase_36000.vtk")
 
-gradient = mesh.compute_derivative("U","qcriterion")
-#gradient.rename_array("gradient","dU")
+mesh = load_mesh(r"D:\NTR\examples\ChannelCase_les\03_Solution\VTK\02_Simcase_160.vtk")
+gradient = mesh.compute_derivative("U", "qcriterion")
 gradient.set_active_scalars("qcriterion")
-gradient = gradient.contour(10)
-a = gradient.threshold_percent([0.4, 0.55])
+plotstuff = gradient.contour(12)
+a = plotstuff.threshold_percent([0.4, 0.55])
 a.plot()
