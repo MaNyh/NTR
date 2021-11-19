eb28e37a84ca30cf3788132a6301700ceb77c204
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-11-19 04:02:39 +0100

    cealnup arguments. rereading settings is not necessary here

--- NTR/postprocessing/profile_loading.py
+++ NTR/postprocessing/profile_loading.py
@@ -11,16 +11,15 @@ from NTR.preprocessing.create_geom import extract_geo_paras
 from NTR.utils.filehandling import yaml_dict_read
 
 
-def extract_profile_from_volmesh(settings_yml, volmesh):
-    settings = yaml_dict_read(settings_yml)
-    alpha = settings["geometry"]["alpha"]
+def extract_profile_from_volmesh(alpha, volmesh):
     bounds = volmesh.bounds
     midspan_z = (bounds[-1] - bounds[-2]) / 2
     z_slice = volmesh.slice(normal="z", origin=(0, 0, midspan_z))
 
     edges = z_slice.extract_feature_edges()
     split = edges.connectivity()
-    profilepoints_ids = [idx for idx, i in enumerate(split["RegionId"]) if i == 0]
+    #todo: currect i==1 not secure. RegionId must have two id's
+    profilepoints_ids = [idx for idx, i in enumerate(split["RegionId"]) if i == 1]
     profilepoints = split.extract_cells(profilepoints_ids)
 
     points, psPoly, ssPoly, ind_vk, ind_hk, midsPoly, metal_angle_vk, metal_angle_hk, camber_angle = extract_geo_paras(
@@ -40,7 +39,7 @@ def extract_profile_from_volmesh(settings_yml, volmesh):
     return psVals, ssVals, points, ind_vk, ind_hk, camber_angle
 
 
-def calc_loading_volmesh(settings_yml):
+def calc_loading_volmesh(settings_yml, verbose=False):
     settings = yaml_dict_read(settings_yml)
     case_path = os.path.dirname(settings_yml)
 
@@ -50,7 +49,8 @@ def calc_loading_volmesh(settings_yml):
 
     volmesh = load_mesh(path_to_volmesh)
 
-    psVals, ssVals, sortedPoints, ind_vk, ind_hk, camber_angle = extract_profile_from_volmesh(settings_yml, volmesh)
+    alpha = settings["geometry"]["alpha"]
+    psVals, ssVals, sortedPoints, ind_vk, ind_hk, camber_angle = extract_profile_from_volmesh(alpha, volmesh)
 
     camber = pv.Line((0, 0, 0), -(sortedPoints[ind_vk] - sortedPoints[ind_hk]))
     #xLine = pv.Line((-1, 0, 0), (1, 0, 0))
@@ -68,17 +68,15 @@ def calc_loading_volmesh(settings_yml):
     psVals = psVals.cell_data_to_point_data()
     ssVals = ssVals.cell_data_to_point_data()
 
-    ps_xc = np.zeros(ssVals.number_of_points)
-    ps_cp = np.zeros(ssVals.number_of_points)
+    ps_xc = np.zeros(psVals.number_of_points)
+    ps_cp = np.zeros(psVals.number_of_points)
 
     for idx, pt in enumerate(psVals.points):
         ps_xc[idx] = pt[0] / camber.length
         ps_cp[idx] = calc_inflow_cp(psVals.point_arrays["Pressure"][idx], 101315, 98000)
 
-    psVals["xc"] = ps_xc
-
     ss_xc = np.zeros(ssVals.number_of_points)
-    ss_cp = np.zeros(psVals.number_of_points)
+    ss_cp = np.zeros(ssVals.number_of_points)
 
     for idx, pt in enumerate(ssVals.points):
         ss_xc[idx] = pt[0] / camber.length
@@ -104,9 +102,9 @@ def calc_loading_volmesh(settings_yml):
             psVals_monotone_xc.append(psVals["xc"][idx])
             psVals_monotone_Pressure.append(psVals["Pressure"][idx])
 
-    plt.figure()
-    plt.plot(ssVals_monotone_xc, ssVals_monotone_Pressure)
-    plt.plot(psVals_monotone_xc, psVals_monotone_Pressure)
-    plt.show()
-
-
+    if verbose:
+        plt.figure()
+        plt.plot(ssVals_monotone_xc, ssVals_monotone_Pressure)
+        plt.plot(psVals_monotone_xc, psVals_monotone_Pressure)
+        plt.show()
+    return psVals, ssVals
