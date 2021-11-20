c0c3a76b29ee699b330331862e58d0dd249988fa
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-11-20 12:47:17 +0100

    outpout Lengthscale-, Reynolds-Stress-, Velocity- and Pressure-profiles

--- NTR/database/datasets/dnsdata_channelflow/onlinedata.py
+++ NTR/database/datasets/dnsdata_channelflow/onlinedata.py
@@ -4,32 +4,34 @@ import tempfile
 import os
 import h5py
 from scipy import integrate
+import matplotlib.pyplot as plt
 
 import NTR
 from NTR.utils.filehandling import yaml_dict_read
 
-#todo: check where to get these values online
-nu = 3.50000e-04 # DNS-Moser
-u_tau = 6.37309e-02 # DNS-Moser
+# todo: check where to get these values online
+nu = 3.50000e-04  # DNS-Moser
+u_tau = 6.37309e-02  # DNS-Moser
+
 
 def get(case_name):
-    basedir = os.path.join(os.path.dirname(NTR.__file__),"database","datasets","dnsdata_channelflow")
-    cases_urls = yaml_dict_read(os.path.join(basedir,"cases.yml"))
+    basedir = os.path.join(os.path.dirname(NTR.__file__), "database", "datasets", "dnsdata_channelflow")
+    cases_urls = yaml_dict_read(os.path.join(basedir, "cases.yml"))
     case_links = cases_urls[case_name]
     tempdir = tempfile.TemporaryDirectory()
     data = {}
     for var, file in case_links.items():
         extension = os.path.splitext(file)[1]
 
-        tmpfile = os.path.join(tempdir.name,var)
+        tmpfile = os.path.join(tempdir.name, var)
         r = requests.get(file, allow_redirects=True)
         print("loaded " + file)
-        if extension==".dat":
+        if extension == ".dat":
             with open(tmpfile, "w") as varf:
                 varf.write(r.content.decode("ascii"))
-            data[var]=np.genfromtxt(tmpfile, comments='%')
-        elif extension==".h5":
-            with open(tmpfile,"wb") as varf:
+            data[var] = np.genfromtxt(tmpfile, comments='%')
+        elif extension == ".h5":
+            with open(tmpfile, "wb") as varf:
                 varf.write(r.content)
             with h5py.File(tmpfile, "r") as hdf:
                 # List all groups
@@ -37,42 +39,41 @@ def get(case_name):
                 ls = list(hdf.keys())
                 for item in ls:
                     h5pydat[item] = np.array(hdf.get(item))
-            data[var]=h5pydat
+            data[var] = h5pydat
     return data
 
 
-def create_bcdata_from_dnsdatabase(case_name):
+def create_bcdata_from_dnsdatabase(case_name,verbose=False):
     simdata = get(case_name)
     DNS_mean = simdata["vel_pressure_mean_prof"]
-    DNS_vel_fluc = simdata["covar_vel_prof"]
     Euu = simdata["energy_spec"]["Euu_kx"]
-    kx = simdata["energy_spec"]["kx"]
-    # TKE is normalized by 1/utau^2
-    TKE_norm = 0.5 * (DNS_vel_fluc[:, 2] + DNS_vel_fluc[:, 3] + DNS_vel_fluc[:, 4])
-    TKE = TKE_norm * u_tau ** 2  # adding dimension via u_tau
-#    DNS_UMean = DNS_mean[:, 2] * u_tau
 
     ###################################### Returning spatial-two-point-correlation & length scale from given spectra
-    dx_range_180 = np.linspace(0, 4.0 * np.pi, len(Euu))  # delta x space for two-point-correlation
+    dx_range = np.linspace(0, 4.0 * np.pi, len(Euu))  # delta x space for two-point-correlation
 
-    L_returned = []
+    Ls = []
+    Rs = [simdata["uu_prof"],simdata["uv_prof"],simdata["vv_prof"],simdata["uu_prof"]*0,simdata["ww_prof"]]
+    Us = simdata["vel_pressure_mean_prof"][::,2]*u_tau
+    Ps = simdata["vel_pressure_mean_prof"][::,5]*u_tau
     Ruu_returned = []
     for Euu_pt in Euu.T:
         Ruu_return_tmp = np.fft.ifft(((Euu_pt)) / 2)
         Ruu_return_real_tmp = (Ruu_return_tmp.real)
         Ruu_returned.append(Ruu_return_real_tmp)
-        L_return_tmp = integrate.simps(Ruu_return_real_tmp / Ruu_return_real_tmp[0],dx_range_180)  # normalizing with Ruu_return_real_tmp[0]
-        L_returned.append(L_return_tmp)
-    L_returned = np.asarray(L_returned)
+        L_return_tmp = integrate.simps(Ruu_return_real_tmp / Ruu_return_real_tmp[0],
+                                       dx_range)  # normalizing with Ruu_return_real_tmp[0]
+        Ls.append(L_return_tmp)
+    Ls = np.asarray(Ls)
 
-    plt.figure()
-    plt.plot(L_returned, DNS_mean[:, 0], '-o', label=case_name + ' - DNS-Moser 2015')
-    plt.legend()
-    plt.xlabel("Lx")
-    plt.ylabel("y/delta")
-    plt.grid()
+    if verbose:
+        plt.figure()
+        plt.plot(Ls, DNS_mean[:, 0], '-o', label=case_name + ' - DNS-Moser 2015')
+        plt.legend()
+        plt.xlabel("L")
+        plt.ylabel("y")
+        plt.grid()
+
+    return Ls, Rs, Us, Ps
 
 
-create_bcdata_from_dnsdatabase("ReTau1000")
-
-
+create_bcdata_from_dnsdatabase("ReTau5200")
