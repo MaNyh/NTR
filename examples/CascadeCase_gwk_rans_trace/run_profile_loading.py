e4e9dd152e1b9fd3cfd0946d01e663e1e6fca806
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-11-21 02:15:10 +0100

    cleanup. values are still messy. delete hardcoded parameters in future commits.

--- examples/CascadeCase_gwk_rans_trace/run_profile_loading.py
+++ examples/CascadeCase_gwk_rans_trace/run_profile_loading.py
@@ -6,25 +6,23 @@ from NTR.postprocessing.profile_loading import calc_inflow_cp
 
 psVals, ssVals = calc_loading_volmesh("case_settings.yml")
 
-#todo: this must be read by a generic function from the volmesh
+# todo: this must be read by a generic function from the volmesh
 fake_p1 = 100449.54753
 fake_p1t = 102148.13498
 
 ps_xc_numerical = psVals["xc"]
 ps_xc_pressure_numerical = psVals["Pressure"]
-ps_xc_cp_numerical = calc_inflow_cp(ps_xc_pressure_numerical,fake_p1t,fake_p1)
-
+ps_xc_cp_numerical = calc_inflow_cp(ps_xc_pressure_numerical, fake_p1t, fake_p1)
 
 ss_xc_numerical = ssVals["xc"]
 ss_xc_pressure_numerical = ssVals["Pressure"]
-ss_xc_cp_numerical = calc_inflow_cp(ss_xc_pressure_numerical,fake_p1t,fake_p1)
-
+ss_xc_cp_numerical = calc_inflow_cp(ss_xc_pressure_numerical, fake_p1t, fake_p1)
 
 ss_xc, ss_cp, ps_xc, ps_cp = read_gilgegwk(verbose=False)
 
 plt.figure()
-plt.plot(ss_cp,ss_xc,label="ss from experiment")
-plt.plot(ps_cp,ps_xc,label="ps from experiment")
-plt.plot(ss_xc_cp_numerical,ss_xc_numerical,label="ss from numeric")
-plt.plot(ps_xc_cp_numerical,ps_xc_numerical,label="ss from numeric")
+plt.plot(ss_xc,ss_cp , label="ss from experiment")
+plt.plot( ps_xc,ps_cp, label="ps from experiment")
+plt.plot(ss_xc_numerical, ss_xc_cp_numerical, label="ss from numeric")
+plt.plot(ps_xc_numerical, ps_xc_cp_numerical, label="ss from numeric")
 plt.show()
