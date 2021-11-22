9c4ebe42ee7b114a45c1bac3698f98d120b07da7
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-11-22 01:22:59 +0100

    implementation of a new curvature-spline-function. which one is better and more accurate?

--- NTR/utils/geom_functions/spline.py
+++ NTR/utils/geom_functions/spline.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, UnivariateSpline
 
 from NTR.utils.geom_functions.vector import midpoint
 
@@ -112,4 +112,42 @@ def splineCurvature(xx,yy):
     d2y_dt2 = np.gradient(dy_dt)
     curvature = np.abs(d2x_dt2 * dy_dt - dx_dt * d2y_dt2) / (dx_dt * dx_dt + dy_dt * dy_dt) ** 1.5
 
+    return curvature
+
+
+def curvature_splines(x, y=None, error=0.0001):
+    """Calculate the signed curvature of a 2D curve at each point
+    using interpolating splines.
+    Parameters
+    ----------
+    x,y: numpy.array(dtype=float) shape (n_points, )
+         or
+         y=None and
+         x is a numpy.array(dtype=complex) shape (n_points, )
+         In the second case the curve is represented as a np.array
+         of complex numbers.
+    error : float
+        The admisible error when interpolating the splines
+    Returns
+    -------
+    curvature: numpy.array shape (n_points, )
+    Note: This is 2-3x slower (1.8 ms for 2000 points) than `curvature_gradient`
+    but more accurate, especially at the borders.
+    """
+
+    # handle list of complex case
+    if y is None:
+        x, y = x.real, x.imag
+
+    t = np.arange(x.shape[0])
+    std = error * np.ones_like(x)
+
+    fx = UnivariateSpline(t, x, k=4, w=1 / np.sqrt(std))
+    fy = UnivariateSpline(t, y, k=4, w=1 / np.sqrt(std))
+
+    x_ = fx.derivative(1)(t)
+    x__ = fx.derivative(2)(t)
+    y_ = fy.derivative(1)(t)
+    y__ = fy.derivative(2)(t)
+    curvature = (x_* y__ - y_* x__) / np.power(x_** 2 + y_** 2, 3 / 2)
     return curvature
