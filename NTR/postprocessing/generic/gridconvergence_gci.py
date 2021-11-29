8770dfa4a434a872497481ce936590c9d6dfb3f3
MaNyh <nyhuis@tfd.uni-hannover.de>
2021-11-30 00:36:31 +0100

    cleanup
    cleanup

--- NTR/postprocessing/generic/gridconvergence_gci.py
+++ NTR/postprocessing/generic/gridconvergence_gci.py
@@ -2,57 +2,35 @@ import pyvista as pv
 import numpy as np
 
 
-mesh_0 = pv.UnstructuredGrid()
-mesh_1 = pv.UnstructuredGrid()
-mesh_2 = pv.UnstructuredGrid()
-
-
 def pEq(x, epsilon32, epsilon21, r21, r32, s):
     return ((np.log(epsilon32 / epsilon21) + np.log(((r21 ** x) - s) / ((r32 ** x) - s))) / np.log(r21) - x)
 
-#todo: mesh_0, mesh_1, mesh_2, postfunc, targetval
-def getGCI():
-    gci = 0
 
-    # todo read from postfunc(targetval)
-    fc3 = 23.263
-    fc2 = 23.165
-    fc1 = 23.151
-
-    # todo: read from meshes
-    N3 = 18576
-    N2 = 74304
-    N1 = 297216
-
-    # todo: read from meshes
-    D = 3
-
-    # todo: argument?
-    Fs = 1.25
+def getGCI(numcell_mesh1, numcell_mesh2, numcell_mesh3,fc1,fc2,fc3,grid_dimension, Fs=1.25):
 
     if ((fc1 > fc2) and (fc2 < fc3)):
-        print(
-            'Error: fci not monoton! Check your input, repeat your grid convergence study, or contact your CFD-expert')
+        print('Error: fci not monoton! ')
+        print('Check your input, repeat your grid convergence study, or contact your CFD-expert')
         return -1
 
     if ((fc1 < fc2) and (fc2 > fc3)):
-        print(
-            'Error: fci not monoton! Check your input, repeat your grid convergence study, or contact your CFD-expert')
+        print('Error: fci not monoton!')
+        print('Check your input, repeat your grid convergence study, or contact your CFD-expert')
         return -1
 
     if ((fc1 == fc2) or (fc2 == fc3)):
-        print(
-            'Error: fci not monoton! Check your input, repeat your grid convergence study, or contact your CFD-expert')
+        print('Error: fci not monoton! ')
+        print('Check your input, repeat your grid convergence study, or contact your CFD-expert')
         return -1
 
-    if ((N1 <= N2) or (N2 <= N3)):
+    if ((numcell_mesh1 <= numcell_mesh2) or (numcell_mesh2 <= numcell_mesh3)):
         print('Error: Number of Grid Nodes illogical! Check your input!')
         return -1
 
     print("input data valid")
 
-    r21 = (N1 / N2) ** (1 / D)
-    r32 = (N2 / N3) ** (1 / D)
+    r21 = (numcell_mesh1 / numcell_mesh2) ** (1 / grid_dimension)
+    r32 = (numcell_mesh2 / numcell_mesh3) ** (1 / grid_dimension)
 
     epsilon32 = (fc3 - fc2)
     epsilon21 = (fc2 - fc1)
@@ -75,7 +53,8 @@ def getGCI():
     A_Flag = GCI_2 / (GCI_1 * (r21 ** p))
 
     if ((A_Flag > 1.15) | (A_Flag < 0.85)):
-        print('Error: Your fc-values are not in the asymptotic range! Refine your grid and repeat grid convergence study, or contact your CFD-expert')
+        print('Error: Your fc-values are not in the asymptotic range!')
+        print('Refine your grid and repeat grid convergence study, or contact your CFD-expert')
 
     GCI_1_p1 = Fs * abs((epsilon21 / fc1)) * 1 / ((r21) - 1)
     GCI_2_p1 = Fs * abs((epsilon32 / fc2)) * 1 / ((r32) - 1)
@@ -87,6 +66,17 @@ def getGCI():
     EERE_2_p1 = abs((f_extra_p1 - fc2) / f_extra_p1)
     EERE_3_p1 = abs((f_extra_p1 - fc3) / f_extra_p1)
 
-    return gci
+    return [GCI_1, GCI_2, GCI_3], [EERE_1, EERE_2, EERE_3], [GCI_1_p1, GCI_2_p1, GCI_3_p1], \
+           [EERE_1_p1, EERE_2_p1, EERE_3_p1]
 
-getGCI()
+mesh3_ncell = 400000
+mesh2_ncell = 1200000
+mesh1_ncell = 3600000
+
+fc3 = 23.263
+fc2 = 23.165
+fc1 = 23.151
+
+grid_dimension = 3
+
+a = getGCI(mesh1_ncell, mesh2_ncell, mesh3_ncell,fc1,fc2,fc3,grid_dimension, Fs=1.25)
